08ace617b5bbd7fb5b89ab290426518f09d13fa8
Rodrigo Burgarelli <rodrigo.burgarelli1@gmail.com>
2014-10-13 15:47:39 -0300

    dados novos

--- js/intencao_voto.js
+++ js/intencao_voto.js
@@ -1,6 +1,6 @@
 //onde está os dados
 path = "http://blog.estadaodados.com/projs/media_edados/"
-//path = ""
+path = ""
 //cria as tabs e deixa ativa a que for informada na url
 jQuery(function() {
   jQuery( "#tabs" ).tabs();
